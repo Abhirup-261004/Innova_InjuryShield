225ef372fe1963813ccaf2a572ae7871997655a3
Abhirup Nandi <abhirupnandi@Abhirups-MacBook-Air.local>
2026-02-18 15:14:11 +0530

    Cors stup for backend deploy

--- injuryshield-server/src/server.js
+++ injuryshield-server/src/server.js
@@ -20,7 +20,7 @@ connectDB();
 // Middleware
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
+const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173","https://your-frontend.vercel.app"];
 
 app.use(
   cors({
